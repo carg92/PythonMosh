748509c1995394ba6e75eb48e7fdb283c3d51e43
Cesar Romero <cesar.arg92@gmail.com>
2019-06-10 19:44:39 -0500

    Obtener el input y asignarlo a un string de resultado

---
+++ GetInput.py
@@ -0,0 +1,10 @@
+#nombre = input('¿Cual es tu nombre? ')
+#print('Hola '+ nombre )
+
+#Ejercicio
+#Ask two questions: person's name and favourite color.
+#Then, print a msg like "name likes color"
+
+name = input('¿Cual es tu nombre? ')
+color = input('¿Cual es tu color favorito? ')
+print('El color favorito de '+ name + ' es el '+ color)
