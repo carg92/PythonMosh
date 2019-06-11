80e837159374efb8162da52108ff22fdee413797
Cesar Romero <cesar.arg92@gmail.com>
2019-06-11 12:51:17 -0500

    Formato para strings

---
+++ FormattedStrings.py
@@ -0,0 +1,8 @@
+first_name = 'Cesar'
+last_name = 'Romero'
+mensaje = first_name + '['+ last_name + '] es programador'
+
+#String con formato
+msg = f'{first_name} [{last_name}] es programador'
+print(mensaje)
+print(msg)
