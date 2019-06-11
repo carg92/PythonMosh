1cbfbfd9a07195e14642d9530a3e5af427210222
Cesar Romero <cesar.arg92@gmail.com>
2019-06-10 20:02:07 -0500

    Conversión de str a int

---
+++ TypeConversion.py
@@ -0,0 +1,17 @@
+#nacimiento = input('Año de nacimiento: ')
+
+
+#edad = 2019 - int(nacimiento)
+
+#print('Tienes ' , edad , ' años de edad')
+
+
+#Ejercicio
+#Escribe el peso en libras y conviertelo a kilogramos, imprime el resultado en la terminal
+peso_libras = input('¿Cual es tu peso en libras? ')
+
+libras_kilogramos = float(peso_libras)/2.2046
+lib_kilogramos = float(peso_libras)* 0.45
+
+print('Tu peso en kilogramos es: ', libras_kilogramos)
+print('Tu peso en kilogramos es: ', lib_kilogramos)
