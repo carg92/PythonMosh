517ad5e38c62219dfc8f17bb85baabbbea22c1c3
Cesar Romero <cesar.arg92@gmail.com>
2019-06-11 12:40:30 -0500

    Strings

---
+++ Strings.py
@@ -0,0 +1,29 @@
+#email = '''
+#Hola Cesar,
+
+#Este es nuestro primer email para ti.
+
+
+#Gracias,
+#El equipo de implementación.
+ 
+ #'''
+
+#Triple ''' se utiliza para textos muy largos
+
+#print(email)
+
+#texto = 'Hola'
+#print(texto[1])
+#print(texto[-1])
+#print(texto[0:3])
+
+
+#imprimir_string =email[:]
+
+#print(imprimir_string)
+
+#Ejercicio
+
+name = 'Jennifer'
+print(name[1:-1])
