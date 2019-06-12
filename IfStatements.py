8afaccb06a3b08d381578cdc0fceadbb796c7382
Cesar Romero <cesar.arg92@gmail.com>
2019-06-11 21:25:26 -0500

    If, else, elif

---
+++ IfStatements.py
@@ -0,0 +1,23 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print('Toma mucha agua')
+    print('Es un día caluroso')
+elif is_cold:
+    print('Toma algo caliente')
+    print('Es un día frío')
+else:
+    print('Es un día agradable')
+
+
+print('Disfruta tu día')
+
+#Ejercicio
+good_credit = False
+price = 1000000
+
+if good_credit:
+    print('Tienes un buen crédito, el monto a pagar sería: $', price *0.10)
+else:
+    print('Tienes un mal crédito, el monto a pagar sería: $', price*0.20)
