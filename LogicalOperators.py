4ea19f4495af12d4643bf194106b16462a0f854e
Cesar Romero <cesar.arg92@gmail.com>
2019-06-11 21:34:39 -0500

    Operadores logicos and, or, and not

---
+++ LogicalOperators.py
@@ -0,0 +1,8 @@
+has_high_income = False
+has_good_credit = True
+has_criminal_record = False
+
+if has_good_credit and has_high_income and not has_criminal_record:
+    print("Eres elegible para un prestamo")
+else:
+    print("No eres elegible para un prestamo")
