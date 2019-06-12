cd2b8833b2d3b51201f56a56889bda64bcbc9c56
Cesar Romero <cesar.arg92@gmail.com>
2019-06-12 17:52:11 -0500

    Ciclo For

---
+++ ForLoops.py
@@ -0,0 +1,11 @@
+#for item in range(5,10,2):
+ #   print(item)
+
+#Ejercicio: Calcular y sumar todos los precios
+precios = [10, 20, 30]
+total = 0
+for price in precios:
+    total += price
+    print(f"El total es: {total}")
+
+
