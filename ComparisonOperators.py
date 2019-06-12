a05437eb55798b356a5a9dc8e1f7a7f86fd8c6af
Cesar Romero <cesar.arg92@gmail.com>
2019-06-12 10:18:21 -0500

    Operadores de comparación <, >, <=, >=

---
+++ ComparisonOperators.py
@@ -0,0 +1,22 @@
+#temperatura = 35
+#if temperatura == 30:
+#    print("Es un día caluroso")
+#else:
+#    print("No es un día caluroso")
+
+#Ejercicio
+
+name = input('¿Cual es tu nombre?')
+longitud = len(name)
+
+if longitud < 3:
+    print('Tu nombre debe contener al menos 3 caracteres')
+elif longitud > 50:
+    print('Tu nombre debe contener máximo 50 caracteres')
+else:
+    print('Tu nombre se ve bien')
+
+
+
+
+
