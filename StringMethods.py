4449efa08a8b9c12a5cf5860264f739140c1c459
Cesar Romero <cesar.arg92@gmail.com>
2019-06-11 20:53:42 -0500

    Métodos para strings: in, upper, lower, replace

---
+++ StringMethods.py
@@ -0,0 +1,11 @@
+curso = 'Python para principiantes'
+
+
+print(curso.upper())
+print(curso.lower())
+
+print(curso)
+
+print(curso.replace('Python', 'Java'))
+
+print('Python' in curso) #Compara si la cadena se encuentra dentro de la variable, es booleano
